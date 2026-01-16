49e1703318f3f9060ce8e7177cda3907d93b26e4
AmurKhoyetsyan <javanewss@gmail.com>
2026-01-16 15:44:49 +0400

    fix everything

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
-class ImageUploader
+class ImageUploadController
 {
     protected $model;
 
     public function __constructor()
+    {}
+
+    public function upload(Request $request)
     {}
 }
